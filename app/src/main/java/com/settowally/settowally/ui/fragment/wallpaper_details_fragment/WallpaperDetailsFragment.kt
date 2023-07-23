b2c759cd8daceb8f973777f129faf71c1e5c4982
Adem Ayar <79112917+AAyar94@users.noreply.github.com>
2023-07-23 10:08:03 +0300

    app icon changed

--- app/src/main/java/com/settowally/settowally/ui/fragment/wallpaper_details_fragment/WallpaperDetailsFragment.kt
+++ app/src/main/java/com/settowally/settowally/ui/fragment/wallpaper_details_fragment/WallpaperDetailsFragment.kt
@@ -1,17 +1,32 @@
 package com.settowally.settowally.ui.fragment.wallpaper_details_fragment
 
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewOutlineProvider
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.settowally.settowally.R
 import com.settowally.settowally.databinding.FragmentWallpaperDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 import eightbitlab.com.blurview.RenderScriptBlur
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+import java.io.InputStream
 
 @AndroidEntryPoint
 class WallpaperDetailsFragment : Fragment() {
@@ -38,9 +53,26 @@ class WallpaperDetailsFragment : Fragment() {
 
         Glide.with(binding.root).load(photoArgs.photo.src.original).placeholder(circularProgressDrawable).into(binding.bigImageView)
 
-
+        setupButtons()
         return binding.root
     }
+
+    private fun setupButtons() {
+        binding.downloadButton.setOnClickListener {
+
+        }
+        binding.shareButton.setOnClickListener {
+
+        }
+        binding.detailsButton.setOnClickListener {
+
+        }
+        binding.setWallpaperButton.setOnClickListener {
+
+        }
+    }
+
+
 
     override fun onDestroy() {
         super.onDestroy()
