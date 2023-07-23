684672d07096e1f965c560f0305cb18676c9a85f
Adem Ayar <79112917+AAyar94@users.noreply.github.com>
2023-07-23 09:53:41 +0300

    new splash screen impl

--- app/src/main/java/com/settowally/settowally/ui/MainActivity.kt
+++ app/src/main/java/com/settowally/settowally/ui/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.settowally.settowally.ui
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.color.MaterialColors
 import com.settowally.settowally.R
 import com.settowally.settowally.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,8 +19,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -29,11 +35,34 @@ class MainActivity : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                R.id.wallpaperDetailsFragment -> binding.bottomNavBar.visibility= View.GONE
-                else -> binding.bottomNavBar.visibility = View.VISIBLE
+                R.id.wallpaperDetailsFragment -> {
+                    binding.bottomNavBar.visibility = View.GONE
+                  //  window.statusBarColor=Color.TRANSPARENT
+                    //window.navigationBarColor=Color.TRANSPARENT
+                }
+
+                else -> {
+                    binding.bottomNavBar.visibility = View.VISIBLE
+                    /*windowBarsColorChanger(
+                        MaterialColors.getColor(
+                            binding.root,
+                            com.google.android.material.R.attr.colorSecondaryContainer
+                        )
+                    )*/
+                }
             }
         }
+    }
 
+    private fun windowBarsColorChanger(color: Int) {
+        window.statusBarColor = MaterialColors.getColor(
+            binding.root,
+            color
+        )
+        window.navigationBarColor = MaterialColors.getColor(
+            binding.root,
+            color
+        )
 
     }
 
