160400f6402a3e82f5d4391c94318f86cc3e7f83
Adem Ayar <79112917+AAyar94@users.noreply.github.com>
2023-07-31 09:05:57 +0300

    worked on animated splash

--- app/src/main/java/com/settowally/settowally/ui/fragment/wallpaper_details_fragment/WallpaperDetailsFragment.kt
+++ app/src/main/java/com/settowally/settowally/ui/fragment/wallpaper_details_fragment/WallpaperDetailsFragment.kt
@@ -1,32 +1,17 @@
 package com.settowally.settowally.ui.fragment.wallpaper_details_fragment
 
-import android.content.ContentValues
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewOutlineProvider
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
-import com.settowally.settowally.R
 import com.settowally.settowally.databinding.FragmentWallpaperDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 import eightbitlab.com.blurview.RenderScriptBlur
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import java.io.IOException
-import java.io.InputStream
 
 @AndroidEntryPoint
 class WallpaperDetailsFragment : Fragment() {
@@ -40,7 +25,7 @@ class WallpaperDetailsFragment : Fragment() {
     ): View {
         mBinding = FragmentWallpaperDetailsBinding.inflate(layoutInflater, container, false)
 
-        binding.blurViewOverlay.setupWith(binding.root,  RenderScriptBlur(requireContext()))
+        binding.blurViewOverlay.setupWith(binding.root, RenderScriptBlur(requireContext()))
             .setFrameClearDrawable(binding.bigImageView.background)
             .setBlurRadius(5f)
         binding.blurViewOverlay.outlineProvider = ViewOutlineProvider.BACKGROUND;
@@ -73,10 +58,8 @@ class WallpaperDetailsFragment : Fragment() {
     }
 
 
-
     override fun onDestroy() {
         super.onDestroy()
-        mBinding=null
+        mBinding = null
     }
-
 }
