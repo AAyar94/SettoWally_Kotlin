4ffbe6f0836f9bf21d6b18074825eec60110821e
Adem Ayar <79112917+AAyar94@users.noreply.github.com>
2023-08-03 20:50:22 +0300

    material alert dialog added for photo details popup

--- app/src/main/java/com/settowally/settowally/ui/fragment/wallpaper_details_fragment/WallpaperDetailsFragment.kt
+++ app/src/main/java/com/settowally/settowally/ui/fragment/wallpaper_details_fragment/WallpaperDetailsFragment.kt
@@ -1,5 +1,6 @@
 package com.settowally.settowally.ui.fragment.wallpaper_details_fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,9 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.settowally.settowally.R
+import com.settowally.settowally.databinding.AlartDialogPhotoDetailBinding
 import com.settowally.settowally.databinding.FragmentWallpaperDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 import eightbitlab.com.blurview.RenderScriptBlur
@@ -36,12 +40,14 @@ class WallpaperDetailsFragment : Fragment() {
         circularProgressDrawable.centerRadius = 50f
         circularProgressDrawable.start()
 
-        Glide.with(binding.root).load(photoArgs.photo.src.original).placeholder(circularProgressDrawable).into(binding.bigImageView)
+        Glide.with(binding.root).load(photoArgs.photo.src.original)
+            .placeholder(circularProgressDrawable).into(binding.bigImageView)
 
         setupButtons()
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     private fun setupButtons() {
         binding.downloadButton.setOnClickListener {
 
@@ -50,7 +56,21 @@ class WallpaperDetailsFragment : Fragment() {
 
         }
         binding.detailsButton.setOnClickListener {
-
+            val builder = MaterialAlertDialogBuilder(requireContext())
+            val alertDialogView = AlartDialogPhotoDetailBinding.inflate(layoutInflater)
+            builder.setView(alertDialogView.root)
+            alertDialogView.alertTitle.text = requireContext().getString(R.string.photo_details)
+            alertDialogView.photoDetailHeight.text =
+                "${requireContext().getString(R.string.height)} ${photoArgs.photo.height}"
+            alertDialogView.photoDetailWidth.text =
+                "${requireContext().getString(R.string.width)} ${photoArgs.photo.width}"
+            alertDialogView.photoDetailPhotographer.text =
+                "${requireContext().getString(R.string.photographer)} ${photoArgs.photo.photographer}"
+            val dialog = builder.create()
+            alertDialogView.buttonCloseAlertDialog.setOnClickListener {
+                dialog.dismiss()
+            }
+            dialog.show()
         }
         binding.setWallpaperButton.setOnClickListener {
 
