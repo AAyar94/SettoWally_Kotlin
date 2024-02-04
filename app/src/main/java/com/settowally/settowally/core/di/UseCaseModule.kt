6caf6f2478359cd3fae197c8724438824676f899
Adem Ayar <ademayar09@gmail.com>
2024-02-04 18:53:43 +0300

    create: UseCaseModule,

---
+++ app/src/main/java/com/settowally/settowally/core/di/UseCaseModule.kt
@@ -0,0 +1,41 @@
+package com.settowally.settowally.core.di
+
+import com.settowally.settowally.data.repository.LocalDataRepositoryImpl
+import com.settowally.settowally.data.repository.RemoteDataDataRepositoryImpl
+import com.settowally.settowally.domain.usecase.GetAllPhotosUseCase
+import com.settowally.settowally.domain.usecase.GetFavoritePhotosUseCase
+import com.settowally.settowally.domain.usecase.GetSinglePhotoUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {
+
+    @Provides
+    @Singleton
+    fun provideGetFavoritePhotosUseCase(
+        repository: LocalDataRepositoryImpl
+    ): GetFavoritePhotosUseCase {
+        return GetFavoritePhotosUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetSinglePhotoUseCase(
+        repository: RemoteDataDataRepositoryImpl
+    ): GetSinglePhotoUseCase {
+        return GetSinglePhotoUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetAllPhotosUseCase(
+        repository: RemoteDataDataRepositoryImpl
+    ): GetAllPhotosUseCase {
+        return GetAllPhotosUseCase(repository)
+    }
+}
