b1d4171e700903a1c57b6e9e6f751eb1e01f5c92
Adem Ayar <ademayar09@gmail.com>
2024-02-04 19:26:13 +0300

    style : add other Quality options in SettingsScreen,
    edit: re-order as a object QualitySelector' s

--- app/src/main/java/com/settowally/settowally/core/util/QualitySelector.kt
+++ app/src/main/java/com/settowally/settowally/core/util/QualitySelector.kt
@@ -1,12 +1,30 @@
 package com.settowally.settowally.core.util
 
-sealed class QualitySelector(name: String) {
-    class Landscape : QualitySelector("landscape")
-    class Large : QualitySelector("large")
-    class Large2x : QualitySelector("large2x")
-    class Medium : QualitySelector("medium")
-    class Original : QualitySelector("original")
-    class Portrait : QualitySelector("portrait")
-    class Small : QualitySelector("small")
-    class Tiny : QualitySelector("tiny")
+sealed class QualitySelector(val name: String) {
+    data object Landscape : QualitySelector("landscape")
+    data object Large : QualitySelector("large")
+    data object Large2x : QualitySelector("large2x")
+    data object Medium : QualitySelector("medium")
+    data object Original : QualitySelector("original")
+    data object Portrait : QualitySelector("portrait")
+    data object Small : QualitySelector("small")
+    data object Tiny : QualitySelector("tiny")
+
+    companion object {
+        fun fromString(name: String): QualitySelector {
+            return when (name) {
+                "landscape" -> QualitySelector.Landscape
+                "large" -> QualitySelector.Large
+                "large2x" -> QualitySelector.Large2x
+                "medium" -> QualitySelector.Medium
+                "original" -> QualitySelector.Original
+                "portrait" -> QualitySelector.Portrait
+                "small" -> QualitySelector.Small
+                "tiny" -> QualitySelector.Tiny
+                else -> {
+                    QualitySelector.Medium
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/settowally/settowally/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/settowally/settowally/presentation/settings/SettingsScreen.kt
@@ -1,8 +1,14 @@
 package com.settowally.settowally.presentation.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,7 +19,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.settowally.settowally.core.theme.LocalSpacing
+import com.settowally.settowally.core.util.QualitySelector
+import com.settowally.settowally.R.string as AppText
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,46 +31,130 @@ fun SettingsScreen(
     viewModel: SettingsViewModel = hiltViewModel()
 ) {
     val uiState = viewModel.uiState.collectAsState()
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Row {
+    val spacing = LocalSpacing.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(),
+            horizontalArrangement = Arrangement.spacedBy(spacing.spaceMedium),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             FilterChip(
                 leadingIcon = {
-                    if (uiState.value.selectedChip == "landscape") {
+                    if (uiState.value.selectedChip == QualitySelector.Landscape.name) {
                         Icon(
                             imageVector = Icons.Default.Check,
                             contentDescription = null
                         )
                     }
                 },
-                selected = uiState.value.selectedChip == "landscape",
-                onClick = { viewModel.updateQualitySetting("landscape") },
-                label = { Text(text = "Landscape") }
+                selected = uiState.value.selectedChip == QualitySelector.Landscape.name,
+                onClick = { viewModel.updateQualitySetting(QualitySelector.Landscape.name) },
+                label = { Text(text = stringResource(id = AppText.landscape)) }
             )
             FilterChip(
                 leadingIcon = {
-                    if (uiState.value.selectedChip == "large") {
+                    if (uiState.value.selectedChip == QualitySelector.Large.name) {
                         Icon(
                             imageVector = Icons.Default.Check,
                             contentDescription = null
                         )
                     }
                 },
-                selected = uiState.value.selectedChip == "large",
-                onClick = { viewModel.updateQualitySetting("large") },
-                label = { Text(text = "Large") }
+                selected = uiState.value.selectedChip == QualitySelector.Large.name,
+                onClick = { viewModel.updateQualitySetting(QualitySelector.Large.name) },
+                label = { Text(text = stringResource(id = AppText.large)) }
             )
             FilterChip(
                 leadingIcon = {
-                    if (uiState.value.selectedChip == "large2x") {
+                    if (uiState.value.selectedChip == QualitySelector.Large2x.name) {
                         Icon(
                             imageVector = Icons.Default.Check,
                             contentDescription = null
                         )
                     }
                 },
-                selected = uiState.value.selectedChip == "large2x",
-                onClick = { viewModel.updateQualitySetting("large2x") },
-                label = { Text(text = "Large2x") }
+                selected = uiState.value.selectedChip == QualitySelector.Large2x.name,
+                onClick = { viewModel.updateQualitySetting(QualitySelector.Large2x.name) },
+                label = { Text(text = stringResource(id = AppText.large2x)) }
+            )
+            FilterChip(
+                leadingIcon = {
+                    if (uiState.value.selectedChip == QualitySelector.Medium.name) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = null
+                        )
+                    }
+                },
+                selected = uiState.value.selectedChip == QualitySelector.Medium.name,
+                onClick = { viewModel.updateQualitySetting(QualitySelector.Medium.name) },
+                label = { Text(text = stringResource(id = AppText.medium)) }
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(),
+            horizontalArrangement = Arrangement.spacedBy(spacing.spaceMedium),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            FilterChip(
+                leadingIcon = {
+                    if (uiState.value.selectedChip == QualitySelector.Original.name) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = null
+                        )
+                    }
+                },
+                selected = uiState.value.selectedChip == QualitySelector.Original.name,
+                onClick = { viewModel.updateQualitySetting(QualitySelector.Original.name) },
+                label = { Text(text = stringResource(id = AppText.original)) }
+            )
+            FilterChip(
+                leadingIcon = {
+                    if (uiState.value.selectedChip == QualitySelector.Portrait.name) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = null
+                        )
+                    }
+                },
+                selected = uiState.value.selectedChip == QualitySelector.Portrait.name,
+                onClick = { viewModel.updateQualitySetting(QualitySelector.Portrait.name) },
+                label = { Text(text = stringResource(id = AppText.portrait)) }
+            )
+            FilterChip(
+                leadingIcon = {
+                    if (uiState.value.selectedChip == QualitySelector.Small.name) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = null
+                        )
+                    }
+                },
+                selected = uiState.value.selectedChip == QualitySelector.Small.name,
+                onClick = { viewModel.updateQualitySetting(QualitySelector.Small.name) },
+                label = { Text(text = stringResource(id = AppText.small)) }
+            )
+            FilterChip(
+                leadingIcon = {
+                    if (uiState.value.selectedChip == QualitySelector.Tiny.name) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = null
+                        )
+                    }
+                },
+                selected = uiState.value.selectedChip == QualitySelector.Tiny.name,
+                onClick = { viewModel.updateQualitySetting(QualitySelector.Tiny.name) },
+                label = { Text(text = stringResource(id = AppText.tiny)) }
             )
         }
     }
